2d34f859083718266267cea7f589c8a31160de01
Jeff Smith <jeff@letote.com>
2017-06-11 19:42:24 -0700

    Remove double quotes from Content-Type header

--- examples/picam_http/lib/picam_http/streamer.ex
+++ examples/picam_http/lib/picam_http/streamer.ex
@@ -24,7 +24,7 @@ defmodule PicamHTTP.Streamer do
   defp send_picture(conn) do
     jpg = Picam.next_frame
     size = byte_size(jpg)
-    header = "------#{@boundary}\r\nContent-Type: \"image/jpeg\"\r\nContent-length: #{size}\r\n\r\n"
+    header = "------#{@boundary}\r\nContent-Type: image/jpeg\r\nContent-length: #{size}\r\n\r\n"
     footer = "\r\n"
     with {:ok, conn} <- chunk(conn, header),
          {:ok, conn} <- chunk(conn, jpg),
