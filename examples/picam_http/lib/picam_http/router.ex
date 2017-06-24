bd5d41b39847fc34cb1ca9e7e1aad20d3c7353f2
Jeff Smith <jeff@letote.com>
2017-06-24 00:39:06 -0700

    Show video stream inside img tag of small html doc

--- examples/picam_http/lib/picam_http/router.ex
+++ examples/picam_http/lib/picam_http/router.ex
@@ -4,7 +4,22 @@ defmodule PicamHTTP.Router do
   plug :match
   plug :dispatch
 
-  forward "/video", to: PicamHTTP.Streamer
+  get "/video" do
+    markup = """
+    <html>
+    <head>
+      <title>Picam Video Stream</title>
+    </head>
+    <body>
+      <img src="video.mjpg" />
+    </body>
+    </html>
+    """
+    put_resp_header(conn, "Content-Type", "text/html")
+    |> send_resp(200, markup)
+  end
+
+  forward "/video.mjpg", to: PicamHTTP.Streamer
 
   match _ do
     send_resp(conn, 404, "oops. Try /video")
