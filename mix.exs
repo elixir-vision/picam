db127aba731bb462a802b3f19674e7b0c56b235f
Jeff Smith <jeff@sohlsmith.com>
2018-02-16 19:02:40 -0600

    Bump version to 0.2.0.

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Picam.Mixfile do
 
   def project do
     [app: :picam,
-     version: "0.2.0-dev",
+     version: "0.2.0",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
