9c6675f93b6701b08b4e98f13260e2b90a6396a6
Jonathan Merriweather <Jono@live.com>
2018-06-26 11:50:50 +1000

    instead of crashing, show offline image

--- lib/picam/camera.ex
+++ lib/picam/camera.ex
@@ -16,10 +16,17 @@ defmodule Picam.Camera do
     port =
       Port.open({:spawn_executable, executable}, [{:packet, 4}, :use_stdio, :binary, :exit_status])
 
-    {:ok, %{port: port, requests: []}}
+    offline_image = image_data("offline_1280_720.jpg")
+
+    {:ok, %{port: port, requests: [], offline: false, offline_image: offline_image}}
   end
 
   # GenServer callbacks
+
+  def handle_call(:next_frame, _from, state = %{offline: true, offline_image: offline_image}) do
+    Task.start(fn -> dispatch(state.requests, offline_image) end)
+    {:reply, offline_image, %{state | requests: []}}
+  end
 
   def handle_call(:next_frame, from, state) do
     state = %{state | requests: [from | state.requests]}
@@ -33,11 +40,19 @@ defmodule Picam.Camera do
 
   def handle_info({_, {:data, jpg}}, state) do
     Task.start(fn -> dispatch(state.requests, jpg) end)
-    {:noreply, %{state | requests: []}}
+    {:noreply, %{state | requests: [], offline: false}}
+  end
+
+  def handle_info(:reconnect_port, state) do
+    executable = Path.join(:code.priv_dir(:picam), "raspijpgs")
+    port = Port.open({:spawn_executable, executable}, [{:packet, 4}, :use_stdio, :binary, :exit_status])
+
+    {:noreply, %{state | port: port}}
   end
 
   def handle_info({_, {:exit_status, _}}, state) do
-    {:stop, :unexpected_exit, state}
+    Process.send_after(self(), :reconnect_port, 10_000)
+    {:noreply, %{state | offline: true}}
   end
 
   def terminate(reason, _state) do
@@ -48,5 +63,11 @@ defmodule Picam.Camera do
 
   defp dispatch(requests, jpg) do
     for req <- Enum.reverse(requests), do: GenServer.reply(req, jpg)
+  end
+
+  defp image_data(filename) when is_binary(filename) do
+    :code.priv_dir(:picam)
+    |> Path.join("fake_camera_images/#{filename}")
+    |> File.read!()
   end
 end
